a6d4d62f53796b7fa586a5b07941de954bff88e3
Ken Detergent <rubbersoulken@gmail.com>
2025-02-09 18:29:53 +0900

    tailwind practice

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza</Link>
+    <header className="bg-yellow-500 uppercase border-b border-stone-200 px-4 py-3">
+      <Link to="/" className="tracking-wider">
+        Fast React Pizza
+      </Link>
       <SearchOrder></SearchOrder>
+      <Username></Username>
     </header>
   );
 }
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,16 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className="mt-8 mb-10 text-center">
+      <h1 className="text-center text-xl font-semibold text-gray-600 uppercase mt-8 mb-10">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-amber-400">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+      <CreateUser></CreateUser>
     </div>
   );
 }
